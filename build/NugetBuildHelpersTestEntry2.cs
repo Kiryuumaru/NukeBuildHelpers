8e92a757c0a596483c287d6d2c8c765b53acb3fb
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-01 17:38:03 +0800

    Revert "Try 16"
    This reverts commit bda3d672579c7c547509fd29de50d190747550dd.

---
+++ build/NugetBuildHelpersTestEntry2.cs
@@ -0,0 +1,31 @@
+﻿using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.DotNet;
+using NukeBuildHelpers;
+using NukeBuildHelpers.Enums;
+using NukeBuildHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _build;
+
+public class NugetBuildHelpersTest2 : AppTestEntry<Build>
+{
+    public override RunsOnType RunsOn => RunsOnType.Ubuntu2204;
+
+    public override Type[] AppEntryTargets => new Type[] { typeof(NugetBuildHelpers2) };
+
+    public override void Prepare(Build nukeBuild)
+    {
+        Console.WriteLine(Name + " Prepare");
+    }
+
+    public override void Run(Build nukeBuild)
+    {
+        DotNetTasks.DotNetTest(_ => _
+            .SetProjectFile(nukeBuild.Solution.NukeBuildHelpers_UnitTest));
+    }
+}
