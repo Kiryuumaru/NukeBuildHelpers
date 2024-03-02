9b153ef12dc2de759fd9063f77697dcaa93f968f
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-02 23:24:32 +0800

    New 18

---
+++ NukeBuildHelpers/BaseNukeBuildHelpers.Targets.Pipeline.cs
@@ -0,0 +1,96 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.Extensions.DependencyModel;
+using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities;
+using Nuke.Common.Utilities.Collections;
+using NukeBuildHelpers.Enums;
+using NukeBuildHelpers.Models;
+using Octokit;
+using Semver;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+using System.Text.Json;
+using YamlDotNet.Core.Tokens;
+
+namespace NukeBuildHelpers;
+
+partial class BaseNukeBuildHelpers
+{
+    public Target PipelinePreSetup => _ => _
+        .Description("To be used by pipeline")
+        .DependsOn(Version)
+        .Executes(() =>
+        {
+            GetOrFail(() => SplitArgs, out var splitArgs);
+            GetOrFail(() => GetAppEntryConfigs(), out var appEntryConfigs);
+
+            IReadOnlyCollection<Output> lsRemote = null;
+
+            List<(string AppId, string Env, SemVersion Version)> toRelease = new();
+
+            foreach (var key in splitArgs.Keys.Any() ? splitArgs.Keys.ToList() : appEntryConfigs.Select(i => i.Key))
+            {
+                string appId = key;
+
+                GetOrFail(appId, appEntryConfigs, out appId, out var appEntry);
+                GetOrFail(() => GetAllVersions(appId, appEntryConfigs, ref lsRemote), out var allVersions);
+
+                if (allVersions.GroupKeySorted.Any())
+                {
+                    foreach (var groupKey in allVersions.GroupKeySorted)
+                    {
+                        string env;
+                        if (string.IsNullOrEmpty(groupKey))
+                        {
+                            env = "main";
+                            if (!Repository.Branch.Equals("master", StringComparison.OrdinalIgnoreCase) &&
+                                !Repository.Branch.Equals("main", StringComparison.OrdinalIgnoreCase) &&
+                                !Repository.Branch.Equals("prod", StringComparison.OrdinalIgnoreCase))
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            env = groupKey;
+                            if (!Repository.Branch.Equals(env, StringComparison.OrdinalIgnoreCase))
+                            {
+                                continue;
+                            }
+                        }
+                        if (!allVersions.LatestVersions.ContainsKey(groupKey) || allVersions.LatestVersions[groupKey] != allVersions.VersionGrouped[groupKey].Last())
+                        {
+                            toRelease.Add((appId, env, allVersions.VersionGrouped[groupKey].Last()));
+                            Log.Information("{appId} Tag: {current}, current latest: {latest}", appId, allVersions.VersionGrouped[groupKey].Last().ToString(), allVersions.LatestVersions.ContainsKey(groupKey) ? allVersions.LatestVersions[groupKey].ToString() : "non");
+                        }
+                        else
+                        {
+                            Log.Information("{appId} Tag: {current}, already latest", appId, allVersions.VersionGrouped[groupKey].Last().ToString());
+                        }
+                    }
+                }
+            }
+
+            foreach (var rel in toRelease)
+            {
+                Log.Information("{appId} on {env} has new version {newVersion}", rel.AppId, rel.Env, rel.Version);
+            }
+
+            PreSetupOutput output = new()
+            {
+                HasRelease = toRelease.Any(),
+                Releases = toRelease.ToDictionary(i => i.AppId, i => new PreSetupOutputVersion() { Environment = i.Env, Version = i.Version.ToString() })
+            };
+
+            var serializedOutput = JsonSerializer.Serialize(output);
+
+            Log.Information("PRE_SETUP_OUTPUT: {output}", serializedOutput);
+
+            Environment.SetEnvironmentVariable("PRE_SETUP_OUTPUT", serializedOutput);
+        });
+}
