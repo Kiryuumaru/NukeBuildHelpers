685b177c1b91c52da6f3177fffd1683167f725bf
Kiryuumaru <kiryuumaru@outlook.com>
2024-03-04 21:39:22 +0800

    Cleanups

--- NukeBuildHelpers.UnitTest/UnitTest1.cs
+++ NukeBuildHelpers.UnitTest/UnitTest1.cs
@@ -3,10 +3,8 @@ namespace NukeBuildHelpers.UnitTest
     public class UnitTest1
     {
         [Fact]
-        public async void Test1()
+        public void Test1()
         {
-            Assert.Equal(1, 1);
-            await Task.Delay(5000);
             Assert.Equal(1, 1);
         }
     }
