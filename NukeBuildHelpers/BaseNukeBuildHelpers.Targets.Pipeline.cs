6c9bb4f35000e774e492536f97640390befb18bf
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-05 14:35:13 +0800

    Test PR 13

--- NukeBuildHelpers/BaseNukeBuildHelpers.Targets.Pipeline.cs
+++ NukeBuildHelpers/BaseNukeBuildHelpers.Targets.Pipeline.cs
@@ -173,7 +173,7 @@ partial class BaseNukeBuildHelpers
             foreach (var buildNumber in buildNumbers.OrderByDescending(i => i))
             {
                 bool hasMatched = false;
-                foreach (var line in Git.Invoke($"branch -r --contains refs/tags/build.{buildNumber}"))
+                foreach (var line in Git.Invoke($"branch -r --contains refs/tags/build.{buildNumber}", logOutput: false, logInvocation: false))
                 {
                     var containBranch = line.Text;
                     containBranch = containBranch[(containBranch.IndexOf('/') + 1)..];
