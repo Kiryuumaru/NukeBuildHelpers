954e566f34bb06a588d80aa477f4d389ccee38e0
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-08 00:46:17 +0800

    Try 142

--- NukeBuildHelpers/Pipelines/GithubPipeline.cs
+++ NukeBuildHelpers/Pipelines/GithubPipeline.cs
@@ -236,7 +236,7 @@ internal class GithubPipeline(BaseNukeBuildHelpers nukeBuild) : IPipeline
         // ██████████████████████████████████████
         var postSetupJob = AddJob(workflow, "post_setup", $"Post Setup", RunsOnType.Ubuntu2204, needs: [.. needs], _if: "success() || failure() || always()");
         AddJobOrStepEnvVarFromNeeds(postSetupJob, "PRE_SETUP_OUTPUT", "pre_setup");
-        AddJobOrStepEnvVar(postSetupJob, "PUBLISH_SUCCESS", "${{ needs.publish.status }}");
+        AddJobOrStepEnvVar(postSetupJob, "PUBLISH_SUCCESS", "${{ needs.publish.result }}");
 
         AddJobStep(postSetupJob, id: "PUBLISH_SUCCESS1", name: $"Output PUBLISH_SUCCESS1",
             run: $"echo \"$PUBLISH_SUCCESS\"");
