c58782ec26aa6309c2d92e0a730d65d48b023232
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-21 17:16:01 +0800

    Try 8

--- NukeBuildHelpers/BaseNukeBuildHelpers.Helpers.cs
+++ NukeBuildHelpers/BaseNukeBuildHelpers.Helpers.cs
@@ -769,7 +769,7 @@ partial class BaseNukeBuildHelpers
         }
         if (lastWidth > Console.CursorLeft)
         {
-            Console.Write(Enumerable.Range(0, lastWidth).Select(i => " "));
+            Console.Write(Enumerable.Range(0, lastWidth).Select(i => " ").Join(""));
         }
         Console.WriteLine(headerSeparator);
         lines++;
