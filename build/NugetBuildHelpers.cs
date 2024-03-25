53528e90c4350053a65dffa6f2a68a2477b83a20
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-26 02:36:15 +0800

    Try 9

--- build/NugetBuildHelpers.cs
+++ build/NugetBuildHelpers.cs
@@ -52,7 +52,6 @@ public class NugetBuildHelpers : AppEntry<Build>
 
     public override void Publish()
     {
-        throw new NotImplementedException();
         DotNetTasks.DotNetNuGetPush(_ => _
             .SetSource("https://nuget.pkg.github.com/kiryuumaru/index.json")
             .SetApiKey(GithubToken)
