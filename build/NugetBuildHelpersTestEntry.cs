7ac2d88af46644acab7820942e60d395979383fd
Kiryuumaru <kiryuumaru@outlook.com>
2024-06-04 02:17:30 +0800

    Try 34

--- build/NugetBuildHelpersTestEntry.cs
+++ build/NugetBuildHelpersTestEntry.cs
@@ -14,34 +14,8 @@ class NugetBuildHelpersTest : AppTestEntry<Build>
 
     public override Type[] AppEntryTargets => [typeof(NugetBuildHelpers)];
 
-    public override AbsolutePath[] CachePaths => [RootDirectory / "samp1", RootDirectory / "samp" / "test.txt"];
-
     public override void Run(AppTestRunContext appTestRunContext)
     {
-        AbsolutePath ascas1 = RootDirectory / "samp1" / "test.txt";
-
-        if (ascas1.FileExists())
-        {
-            Console.WriteLine("1OLD VALLLVALLLLLLVALLLdsdLLLs1: " + ascas1.ReadAllText());
-        }
-
-        string newVal1 = Guid.NewGuid().Encode();
-        Console.WriteLine("1NEW VALLLLLLVALLLLLL: " + newVal1);
-        ascas1.Parent.CreateDirectory();
-        ascas1.WriteAllText(newVal1);
-
-        AbsolutePath ascas = RootDirectory / "samp" / "test.txt";
-
-        if (ascas.FileExists())
-        {
-            Console.WriteLine("OLD VALLLVALLLLLLVALLLdsdLLLs: " + ascas.ReadAllText());
-        }
-
-        string newVal = Guid.NewGuid().Encode();
-        Console.WriteLine("NEW VALLLLLLVALLLLLL: " + newVal);
-        ascas.Parent.CreateDirectory();
-        ascas.WriteAllText(newVal);
-
         DotNetTasks.DotNetClean(_ => _
             .SetProject(RootDirectory / "NukeBuildHelpers.UnitTest" / "NukeBuildHelpers.UnitTest.csproj"));
         DotNetTasks.DotNetTest(_ => _
