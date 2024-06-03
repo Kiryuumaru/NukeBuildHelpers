16d290ba0240301c8002d85940d1c40fd3b1ab41
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-06-03 13:22:31 +0800

    Added release on pull request feature (#37)
    * Added release on pull request feature
    
    * Common code
    
    * Merge branch 'prerelease' into feat/release-on-pr 2

--- NukeBuildHelpers/BaseNukeBuildHelpers.Helpers.cs
+++ NukeBuildHelpers/BaseNukeBuildHelpers.Helpers.cs
@@ -378,7 +378,7 @@ partial class BaseNukeBuildHelpers
         return Task.WhenAll(tasks);
     }
 
-    private async Task<List<(AppEntry AppEntry, AllVersions AllVersions, SemVersion BumpVersion)>> StartBump()
+    private async Task<List<(AppEntry AppEntry, AllVersions AllVersions, SemVersion BumpVersion)>> InteractiveRelease()
     {
         Prompt.ColorSchema.Answer = ConsoleColor.Green;
         Prompt.ColorSchema.Select = ConsoleColor.DarkMagenta;
@@ -469,7 +469,7 @@ partial class BaseNukeBuildHelpers
             Console.Write("  Current latest version: ");
             ConsoleHelpers.WriteWithColor(currentEnvLatestVersion?.LastOrDefault()?.ToString() ?? "null", ConsoleColor.Green);
             Console.WriteLine("");
-            var bumpVersionStr = Prompt.Input<string>("New Version", validators: [Validators.Required(),
+            List<Func<object, ValidationResult?>> validators = [Validators.Required(),
                     (input => {
                         if (!SemVersion.TryParse(input.ToString(), SemVersionStyles.Strict, out var inputVersion))
                         {
@@ -511,10 +511,53 @@ partial class BaseNukeBuildHelpers
                         }
 
                         return ValidationResult.Success;
-                    })]);
+                    })];
+            
+            var bumpVersionStr = await Task.Run(() => Prompt.Input<string>("New Version", validators: validators));
             var bumpVersion = SemVersion.Parse(bumpVersionStr, SemVersionStyles.Strict);
             appEntryVersionsToBump.Add((appEntryVersion.AppEntry, appEntryVersion.AllVersions, bumpVersion));
         }
+
+        return appEntryVersionsToBump;
+    }
+
+    private async Task<List<(AppEntry AppEntry, AllVersions AllVersions, SemVersion BumpVersion)>> StartRelease()
+    {
+        var appEntryVersionsToBump = await InteractiveRelease();
+
+        if (appEntryVersionsToBump.Count == 0)
+        {
+            Log.Information("No version selected to bump.");
+            return appEntryVersionsToBump;
+        }
+
+        List<string> tagsToPush = [];
+
+        foreach (var appEntryVersionToBump in appEntryVersionsToBump)
+        {
+            if (appEntryVersionToBump.AppEntry.MainRelease)
+            {
+                tagsToPush.Add(appEntryVersionToBump.BumpVersion.ToString() + "-bump");
+            }
+            else
+            {
+                tagsToPush.Add(appEntryVersionToBump.AppEntry.Id.ToLowerInvariant() + "/" + appEntryVersionToBump.BumpVersion.ToString() + "-bump");
+            }
+        }
+        // ---------- Apply bump ----------
+
+        await Task.Run(() =>
+        {
+            var bumpTag = "bump-" + Repository.Branch.ToLowerInvariant();
+            Git.Invoke("checkout -b rel/" + bumpTag, logInvocation: false, logOutput: false);
+        });
+
+        return appEntryVersionsToBump;
+    }
+
+    private async Task<List<(AppEntry AppEntry, AllVersions AllVersions, SemVersion BumpVersion)>> StartBump()
+    {
+        var appEntryVersionsToBump = await InteractiveRelease();
 
         if (appEntryVersionsToBump.Count == 0)
         {
