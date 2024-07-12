dabcf8b1f114386e87de3bd12f8ec961d2f98836
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-07-12 11:34:23 +0800

    Added older LTS runners (#96)

---
+++ NukeBuildHelpers/Runner/Models/RunnerOSUbuntu2004.cs
@@ -0,0 +1,31 @@
+﻿using NukeBuildHelpers.Pipelines.Azure.Models;
+using NukeBuildHelpers.Pipelines.Common.Enums;
+using NukeBuildHelpers.Pipelines.Github.Models;
+using NukeBuildHelpers.Runner.Abstraction;
+
+namespace NukeBuildHelpers.Runner.Models;
+
+internal class RunnerOSUbuntu2004 : RunnerOS
+{
+    public override string Name { get; } = "ubuntu-20.04";
+
+    public override object GetPipelineOS(PipelineType pipelineType)
+    {
+        return pipelineType switch
+        {
+            PipelineType.Azure => new RunnerAzurePipelineOS() { PoolName = "Azure Pipelines", PoolVMImage = "ubuntu-20.04" },
+            PipelineType.Github => new RunnerGithubPipelineOS() { RunsOn = "ubuntu-20.04" },
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public override string GetRunScript(PipelineType pipelineType)
+    {
+        return pipelineType switch
+        {
+            PipelineType.Azure => "chmod +x ./build.sh && ./build.sh",
+            PipelineType.Github => "chmod +x ./build.sh && ./build.sh",
+            _ => throw new NotImplementedException()
+        };
+    }
+}
---
+++ NukeBuildHelpers/Runner/Models/RunnerOSWindows2019.cs
@@ -0,0 +1,31 @@
+﻿using NukeBuildHelpers.Pipelines.Azure.Models;
+using NukeBuildHelpers.Pipelines.Common.Enums;
+using NukeBuildHelpers.Pipelines.Github.Models;
+using NukeBuildHelpers.Runner.Abstraction;
+
+namespace NukeBuildHelpers.Runner.Models;
+
+internal class RunnerOSWindows2019 : RunnerOS
+{
+    public override string Name { get; } = "windows-2019";
+
+    public override object GetPipelineOS(PipelineType pipelineType)
+    {
+        return pipelineType switch
+        {
+            PipelineType.Azure => new RunnerAzurePipelineOS() { PoolName = "Azure Pipelines", PoolVMImage = "windows-2019" },
+            PipelineType.Github => new RunnerGithubPipelineOS() { RunsOn = "windows-2019" },
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public override string GetRunScript(PipelineType pipelineType)
+    {
+        return pipelineType switch
+        {
+            PipelineType.Azure => "./build.cmd",
+            PipelineType.Github => "./build.cmd",
+            _ => throw new NotImplementedException()
+        };
+    }
+}
