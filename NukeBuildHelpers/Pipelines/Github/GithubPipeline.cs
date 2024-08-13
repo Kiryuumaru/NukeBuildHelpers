91367902b875a7436667428dae02ddf227743db5
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-08-13 10:08:52 +0800

    Added persistent credentials to each checkout steps

--- NukeBuildHelpers/Pipelines/Github/GithubPipeline.cs
+++ NukeBuildHelpers/Pipelines/Github/GithubPipeline.cs
@@ -458,6 +458,7 @@ internal class GithubPipeline(BaseNukeBuildHelpers nukeBuild) : IPipeline
         AddJobStepWith(step, "fetch-depth", fetchDepth.ToString());
         AddJobStepWith(step, "fetch-tags", fetchTags ? "true" : "false");
         AddJobStepWith(step, "submodules", GetSubmoduleCheckoutType(submoduleCheckoutType));
+        AddJobStepWith(step, "persist-credentials", "true");
 
         return step;
     }
@@ -468,6 +469,7 @@ internal class GithubPipeline(BaseNukeBuildHelpers nukeBuild) : IPipeline
         AddJobStepWith(step, "fetch-depth", GetImportedEnvVarExpression(entryId, "CHECKOUT_FETCH_DEPTH"));
         AddJobStepWith(step, "fetch-tags", GetImportedEnvVarExpression(entryId, "CHECKOUT_FETCH_TAGS"));
         AddJobStepWith(step, "submodules", GetImportedEnvVarExpression(entryId, "CHECKOUT_SUBMODULES"));
+        AddJobStepWith(step, "persist-credentials", "true");
 
         return step;
     }
